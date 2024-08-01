6ba960e980ac94df0c03112c436f1b258c77d825
Geo <50969430+GeoGeoGeoGeoGeo@users.noreply.github.com>
2024-08-01 20:27:32 +0200

    Python and data.json

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("God_Bless")
 
-#test_hello
+import json
+
+with open("data.json") as file:
+    data = json.load(file)
+    data.append(
+        {
+            "name": "Any",
+            "age": "30",
+            "height": 159,
+            "gender": "female"
+        }
+    )
+print(data)
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
